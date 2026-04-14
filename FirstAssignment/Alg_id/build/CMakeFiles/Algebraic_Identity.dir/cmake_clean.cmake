99e76b60343c53669a1af7d846a29f2d035e4611
annareds <annarossi260204@gmail.com>
2026-04-14 12:55:51 +0200

    Add files via upload

---
+++ FirstAssignment/Alg_id/build/CMakeFiles/Algebraic_Identity.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Algebraic_Identity.dir/Algebraic_Identity.cpp.o"
+  "CMakeFiles/Algebraic_Identity.dir/Algebraic_Identity.cpp.o.d"
+  "libAlgebraic_Identity.pdb"
+  "libAlgebraic_Identity.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Algebraic_Identity.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ FirstAssignment/Alg_id/build/CMakeFiles/Algebraic_Identity.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Algebraic_Identity.
+# This may be replaced when dependencies are built.
